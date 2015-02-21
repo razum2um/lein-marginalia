ac24ef46ca734908c9c57f3fe2ff787b9166e617
Vlad Bokov <razum2um@mail.ru>
2015-02-21 23:04:43 +0600

    update deps

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject lein-marginalia "0.8.1-SNAPSHOT"
+(defproject org.clojars.razum2um/lein-marginalia "0.8.1-SNAPSHOT"
   :description "Leiningen plugin for Marginalia."
   :url "http://github.com/fogus/marginalia"
-  :dependencies [[marginalia "0.8.1-SNAPSHOT"]]
+  :dependencies [[org.clojars.razum2um/marginalia "0.8.1-SNAPSHOT"]]
   :eval-in :leiningen)
 
